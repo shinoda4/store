69b23c33b73eec2bd4b329663afed5290010f807
desonglll <lindesong666@163.com>
2025-08-28 00:32:35 +0800

    first commit.

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get "subscribers/create"
+  resource :session
+  resources :passwords, param: :token
+  get "up" => "rails/health#show", as: :rails_health_check
+  root "products#index"
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
+end
---
+++ test/controllers/subscriber_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class SubscriberControllerTest < ActionDispatch::IntegrationTest
+  # test "should get create" do
+  #   get subscriber_create_url
+  #   assert_response :success
+  # end
+end
